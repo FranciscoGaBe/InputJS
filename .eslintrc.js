8bb035fc945959da9622bdd50d51d854e95f4801
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-03-11 19:09:37 +0100

    Chore: added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'max-len': ['warn', {
+      code: 80,
+    }]
+  },
+  settings: {},
+};
