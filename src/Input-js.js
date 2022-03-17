e8cae64995aa78d3eab4cb3a5c3089976ef6b2ca
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-03-17 03:59:14 +0100

    Feat: added joystick

--- src/Input-js.js
+++ src/Input-js.js
@@ -1,21 +1,38 @@
 /**
  *
  * @typedef {{
+ *  x: number,
+ *  y: number
+ * }} Vector2
+ *
+ * @typedef {{
  *  lastKeyPressed: string,
  *  [key: string]: boolean
  * }} Keys
  *
  * @typedef {{
- *  position: {
- *    x: number,
- *    y: number
- *  },
+ *  position: Vector2,
  *  [key: string]: boolean
  * }} Mouse
  *
  * @typedef {{
+ *  active: boolean,
+ *  start: Vector2,
+ *  current: Vector2,
+ *  move: Vector2
+ *  vertical: number,
+ *  horizontal: number,
+ *  normalized: Vector2,
+ * }} Joystick
+ *
+ * @typedef {{
+ *  threshold: number
+ * }} Options
+ *
+ * @typedef {{
  *  readonly keys: Keys,
  *  readonly mouse: Mouse,
+ *  readonly joystick: Joystick,
  *  destroy: () => void
  * }} InputJSInstance
  */
@@ -28,30 +45,120 @@ const generateProxy = (obj) => new Proxy(obj, {
   },
 });
 
-/*
+const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
+const getMagnitude = (x, y) => Math.sqrt(x ** 2 + y ** 2);
+const getZeroPosition = () => ({ x: 0, y: 0 });
+
+/**
  * @param {HTMLElement} element
+ * @param {Options} options
  * @returns {InputJSInstance}
  */
-const InputJS = (element) => {
+const InputJS = (element, options = {}) => {
   /** @type {Keys} */
   const keys = generateProxy({ lastKeyPressed: '' });
   /** @type {Mouse} */
-  const mouse = generateProxy({ position: { x: 0, y: 0 } });
+  const mouse = generateProxy({ position: getZeroPosition() });
+  /** @type {Joystick} */
+  const joystick = {
+    active: false,
+    start: getZeroPosition(),
+    current: getZeroPosition(),
+    move: getZeroPosition(),
+    vertical: 0,
+    horizontal: 0,
+    normalized: getZeroPosition(),
+  };
+
+  const verticalKeys = [
+    { code: 'KeyW', value: -1 },
+    { code: 'KeyS', value: 1 },
+    { code: 'ArrowUp', value: -1 },
+    { code: 'ArrowDown', value: 1 },
+  ];
+  const horizontalKeys = [
+    { code: 'KeyA', value: -1 },
+    { code: 'KeyD', value: 1 },
+    { code: 'ArrowLeft', value: -1 },
+    { code: 'ArrowRight', value: 1 },
+  ];
+  const setAxis = () => {
+    const getMouseJoystick = (axis) => {
+      if (!options.threshold) return 0;
+      const move = joystick.move[axis === 'vertical' ? 'y' : 'x'];
+      const direction = Math.sign(move);
+      return Math.min(Math.abs(move) / options.threshold, 1) * direction;
+    };
+    const vertical = clamp(
+      verticalKeys.reduce(
+        (a, b) => a + (keys[b.code] ? b.value : 0),
+        getMouseJoystick('vertical'),
+      ),
+      -1,
+      1,
+    );
+    const horizontal = clamp(
+      horizontalKeys.reduce(
+        (a, b) => a + (keys[b.code] ? b.value : 0),
+        getMouseJoystick('horizontal'),
+      ),
+      -1,
+      1,
+    );
+    const magnitude = getMagnitude(horizontal, vertical) || 1;
+    const getNormalized = (value) => {
+      const abs = Math.abs(value);
+      const normalized = Math.floor((abs * 100) / magnitude) / 100;
+      return clamp(abs, 0, normalized) * Math.sign(value);
+    };
+    joystick.vertical = vertical;
+    joystick.horizontal = horizontal;
+    joystick.normalized = {
+      x: getNormalized(horizontal),
+      y: getNormalized(vertical),
+    };
+  };
+
+  const getMousePosition = ({ clientX, clientY }) => ({
+    x: clientX - (element.offsetLeft || 0),
+    y: clientY - (element.offsetTop || 0),
+  });
 
   /** @type {Record<string, (event: Event) => void>} */
   const events = {
     keydown: ({ code }) => {
       keys[code] = true;
       keys.lastKeyPressed = code;
+      setAxis();
     },
-    keyup: ({ code }) => { keys[code] = false; },
-    mousedown: ({ button }) => { mouse[button] = true; },
-    mouseup: ({ button }) => { mouse[button] = false; },
-    mousemove: ({ clientX, clientY, currentTarget }) => {
-      mouse.position = {
-        x: clientX - (currentTarget.offsetLeft || 0),
-        y: clientY - (currentTarget.offsetTop || 0),
-      };
+    keyup: ({ code }) => {
+      keys[code] = false;
+      setAxis();
+    },
+    mousedown: (event) => {
+      mouse[event.button] = true;
+      joystick.active = true;
+      joystick.start = getMousePosition(event);
+      setAxis();
+    },
+    mouseup: ({ button }) => {
+      mouse[button] = false;
+      joystick.active = false;
+      joystick.move = getZeroPosition();
+      joystick.start = getZeroPosition();
+      joystick.current = getZeroPosition();
+      setAxis();
+    },
+    mousemove: (event) => {
+      mouse.position = getMousePosition(event);
+      if (joystick.active) {
+        joystick.current = getMousePosition(event);
+        joystick.move = {
+          x: joystick.current.x - joystick.start.x,
+          y: joystick.current.y - joystick.start.y,
+        };
+        setAxis();
+      }
     },
     blur: () => {
       const toReset = [keys, mouse];
@@ -71,6 +178,7 @@ const InputJS = (element) => {
   return {
     get keys() { return keys; },
     get mouse() { return mouse; },
+    get joystick() { return joystick; },
     destroy: () => {
       Object.entries(events).forEach(([type, cb]) => {
         const target = type.slice(0, 3) === 'key' ? document : element;
--- src/Input-js.test.js
+++ src/Input-js.test.js
@@ -17,7 +17,7 @@ const keyboardEvent = (type, code) => new KeyboardEvent(type, { code });
 const fireEvent = (device, type, data) => {
   const getEvent = () => {
     switch (device) {
-      case 'keyboard': return new KeyboardEvent(type, { code: data });
+      case 'keyboard': return new KeyboardEvent(type, data);
       case 'mouse': return new MouseEvent(type, data);
       default: return new Event(type, { details: data });
     }
@@ -80,6 +80,92 @@ describe('InputJS', () => {
     it('keeps track of current mouse position', () => {
       fireEvent('mouse', 'mousemove', { clientX: 20, clientY: 30 });
       expect(inputjs.mouse.position).toEqual({ x: 20, y: 30 });
+    });
+  });
+  describe('.joystick', () => {
+    it('has an .active property to know if it\'s active', () => {
+      expect(inputjs.joystick.active).toBe(false);
+      fireEvent('mouse', 'mousedown');
+      expect(inputjs.joystick.active).toBe(true);
+      fireEvent('mouse', 'mouseup');
+      expect(inputjs.joystick.active).toBe(false);
+    });
+    it('keeps track of starting position', () => {
+      fireEvent('mouse', 'mousedown', { clientX: 20, clientY: 30 });
+      expect(inputjs.joystick.start).toEqual({ x: 20, y: 30 });
+    });
+    it('keeps track of current position', () => {
+      fireEvent('mouse', 'mousedown', { clientX: 10, clientY: 10 });
+      fireEvent('mouse', 'mousemove', { clientX: 20, clientY: 30 });
+      expect(inputjs.joystick.current).toEqual({ x: 20, y: 30 });
+    });
+    it('keeps track of horizontal and vertical move', () => {
+      fireEvent('mouse', 'mousedown', { clientX: 10, clientY: 10 });
+      fireEvent('mouse', 'mousemove', { clientX: 20, clientY: 30 });
+      expect(inputjs.joystick.move).toEqual({ x: 10, y: 20 });
+    });
+    it('passing threshold in options makes the mouse act as joystick', () => {
+      inputjs.destroy();
+      inputjs = InputJS(document, { threshold: 300 });
+      fireEvent('mouse', 'mousedown', { clientX: 0, clientY: 0 });
+      expect(inputjs.joystick.horizontal).toBe(0);
+      fireEvent('mouse', 'mousemove', { clientX: 150, clientY: 150 });
+      expect(inputjs.joystick.horizontal).toBe(0.5);
+    });
+    it('when inactive, reset move, starting and end positions', () => {
+      const zeroPosition = { x: 0, y: 0 };
+      fireEvent('mouse', 'mousedown', { clientX: 10, clientY: 10 });
+      fireEvent('mouse', 'mousemove', { clientX: 20, clientY: 30 });
+      fireEvent('mouse', 'mouseup', { clientX: 20, clientY: 30 });
+      expect(inputjs.joystick.start).toEqual(zeroPosition);
+      expect(inputjs.joystick.current).toEqual(zeroPosition);
+      expect(inputjs.joystick.move).toEqual(zeroPosition);
+    });
+    describe('.vertical', () => {
+      it('returns -1 if KeyW or ArrowUp is pressed', () => {
+        expect(inputjs.joystick.vertical).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'KeyW' });
+        expect(inputjs.joystick.vertical).toBe(-1);
+        fireEvent('keyboard', 'keyup', { code: 'KeyW' });
+        expect(inputjs.joystick.vertical).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'ArrowUp' });
+        expect(inputjs.joystick.vertical).toBe(-1);
+      });
+      it('returns 1 if KeyS or ArrowDown is pressed', () => {
+        expect(inputjs.joystick.vertical).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'KeyS' });
+        expect(inputjs.joystick.vertical).toBe(1);
+        fireEvent('keyboard', 'keyup', { code: 'KeyS' });
+        expect(inputjs.joystick.vertical).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'ArrowDown' });
+        expect(inputjs.joystick.vertical).toBe(1);
+      });
+    });
+    describe('.horizontal', () => {
+      it('returns -1 if KeyA or ArrowLeft is pressed', () => {
+        expect(inputjs.joystick.horizontal).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'KeyA' });
+        expect(inputjs.joystick.horizontal).toBe(-1);
+        fireEvent('keyboard', 'keyup', { code: 'KeyA' });
+        expect(inputjs.joystick.horizontal).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'ArrowLeft' });
+        expect(inputjs.joystick.horizontal).toBe(-1);
+      });
+      it('returns 1 if KeyD or ArrowRight is pressed', () => {
+        expect(inputjs.joystick.horizontal).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'KeyD' });
+        expect(inputjs.joystick.horizontal).toBe(1);
+        fireEvent('keyboard', 'keyup', { code: 'KeyD' });
+        expect(inputjs.joystick.horizontal).toBe(0);
+        fireEvent('keyboard', 'keydown', { code: 'ArrowRight' });
+        expect(inputjs.joystick.horizontal).toBe(1);
+      });
+    });
+    it('has a normalized vector of horizontal and vertical', () => {
+      expect(inputjs.joystick.normalized).toEqual({ x: 0, y: 0 });
+      fireEvent('keyboard', 'keydown', { code: 'KeyD' });
+      fireEvent('keyboard', 'keydown', { code: 'KeyS' });
+      expect(inputjs.joystick.normalized).toEqual({ x: 0.7, y: 0.7 });
     });
   });
 });
